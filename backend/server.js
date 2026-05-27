36605be87bde7c87f9d11887be64aa2343bfd3ab
Christian Saavedra <chrisosami86@gmail.com>
2026-05-27 09:52:34 -0500

    Actualizando server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-require('dotenv').config();
-process.env.TZ = 'America/Bogota';
+require("dotenv").config();
+process.env.TZ = "America/Bogota";
 
-const app = require('./src/app');
+const app = require("./src/app");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log({
+    iso: new Date().toISOString(),
+    local: new Date().toString(),
+    tz: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  });
 });

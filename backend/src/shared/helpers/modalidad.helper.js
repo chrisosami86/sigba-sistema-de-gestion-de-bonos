f922f12e9cf48f0e97197bb8ea33aecaa46314a8
Christian Saavedra <chrisosami86@gmail.com>
2026-05-29 12:10:37 -0500

    Actualizando modalidad operacional

--- backend/src/shared/helpers/modalidad.helper.js
+++ backend/src/shared/helpers/modalidad.helper.js
@@ -1,21 +1,29 @@
 const getModalidadExpression = () => {
   return `
     CASE
-      WHEN r.tipo_asignacion = 'ADMINISTRATIVA'
-        THEN 'venta_libre'
-      WHEN cb.tipo = 'almuerzo'
-        AND r.hora_solicitud::time BETWEEN TIME '08:00' AND TIME '10:15'
-        THEN 'subsidiado'
-      WHEN cb.tipo = 'almuerzo'
-        AND r.hora_solicitud::time BETWEEN TIME '11:30' AND TIME '12:05'
-        THEN 'venta_libre'
-      WHEN cb.tipo = 'refrigerio'
-        AND r.hora_solicitud::time BETWEEN TIME '17:00' AND TIME '18:29'
-        THEN 'subsidiado'
-      WHEN cb.tipo = 'refrigerio'
-        AND r.hora_solicitud::time BETWEEN TIME '18:30' AND TIME '22:00'
-        THEN 'venta_libre'
-      ELSE 'desconocida'
+      WHEN r.modalidad_operacional IS NOT NULL THEN
+        CASE
+          WHEN r.modalidad_operacional = 'administrativo' THEN 'venta_libre'
+          ELSE r.modalidad_operacional
+        END
+      ELSE
+        CASE
+          WHEN r.tipo_asignacion = 'ADMINISTRATIVA'
+            THEN 'venta_libre'
+          WHEN cb.tipo = 'almuerzo'
+            AND r.hora_solicitud::time BETWEEN TIME '08:00' AND TIME '10:15'
+            THEN 'subsidiado'
+          WHEN cb.tipo = 'almuerzo'
+            AND r.hora_solicitud::time BETWEEN TIME '11:30' AND TIME '12:05'
+            THEN 'venta_libre'
+          WHEN cb.tipo = 'refrigerio'
+            AND r.hora_solicitud::time BETWEEN TIME '17:00' AND TIME '18:29'
+            THEN 'subsidiado'
+          WHEN cb.tipo = 'refrigerio'
+            AND r.hora_solicitud::time BETWEEN TIME '18:30' AND TIME '22:00'
+            THEN 'venta_libre'
+          ELSE 'desconocida'
+        END
     END
   `;
 };

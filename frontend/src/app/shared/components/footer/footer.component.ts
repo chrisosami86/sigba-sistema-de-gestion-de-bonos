e7f46cba0f30d0716c46cbceee7e79ad6928846e
Christian Saavedra <chrisosami86@gmail.com>
2026-06-24 15:42:47 -0500

    Actualizando redes sociales

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -3,10 +3,11 @@ import { Facebook } from "../icons/facebook/facebook";
 import { Instagram } from "../icons/instagram/instagram";
 import { Youtube } from "../icons/youtube/youtube";
 import { Internet } from "../icons/internet/internet";
+import { Tiktok } from "../icons/tiktok/tiktok";
 
 @Component({
   selector: 'footer-component',
-  imports: [Facebook, Instagram, Youtube, Internet],
+  imports: [Facebook, Instagram, Youtube, Internet, Tiktok],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent { }
